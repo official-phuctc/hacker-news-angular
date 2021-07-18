451b0eda222c376097a399c2ab2be17ed65cfcdc
Phuc Trinh <trinhcanhphuc@gmail.com>
2021-07-18 19:39:18 +0700

    add news view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hacker-news-angular';
+  block_articles: any = [];
+ 
+  constructor(private http: Http) {
+    http.get('https://hacker-news-rails.herokuapp.com/articles.json')
+      .subscribe(res => {
+        const all_articles = res.json().original_hash.data.articles;
+        while (all_articles.length > 0) {
+          this.block_articles.push(all_articles.splice(0, 4));  
+        }
+      });
+  }
 }
